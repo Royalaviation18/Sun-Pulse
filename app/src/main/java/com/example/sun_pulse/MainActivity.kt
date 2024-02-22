4b8cb01f6dd5aa6aed5e7310e8007d202ed3c08c
Rohit Priyadarshi <56641101+Royalaviation18@users.noreply.github.com>
2024-02-22 13:20:33 +0530

    API Implemented

--- app/src/main/java/com/example/sun_pulse/MainActivity.kt
+++ app/src/main/java/com/example/sun_pulse/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.sun_pulse
 
+import WeatherApp
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+//4fd9bb5002bd73f12c01588fc61fb2f0
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fetchWeatherData()
+    }
+
+    private fun fetchWeatherData() {
+        //all other data can change but the baseurl remains same for all the calls
+        val retrofit = Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .build().create(ApiInterface::class.java)
+        //here units is used to convert into string as metric and then perform the search
+        val response = retrofit.getWeatherData("jaipur","4fd9bb5002bd73f12c01588fc61fb2f0","metric")
+        response.enqueue(object: Callback<WeatherApp>{
+            override fun onResponse(call: Call<WeatherApp>, response: Response<WeatherApp>) {
+                val responseBody = response.body()
+                if (response.isSuccessful && responseBody != null){
+                    val temperature = responseBody.main.temp.toString()
+                    Log.d("TAG", "onResponse: $temperature")
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherApp>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
